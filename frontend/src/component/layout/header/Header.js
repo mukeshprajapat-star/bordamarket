951ebc3d3143f65054e983ef75a3a0c78b75f811
mukesh prajapat <mukeshprajapat7529@gmail.com>
2022-02-22 13:08:56 +0530

    fixed bugs

--- frontend/src/component/layout/header/Header.js
+++ frontend/src/component/layout/header/Header.js
@@ -1,6 +1,5 @@
 import React ,{useState}from 'react';
 import { Menubar } from 'primereact/menubar';
-//import logo from '../../../images/ecommerce.png'
 import logo from "./logoE.jpg"
 import "./Header.css"
 import {InputText} from "primereact/inputtext"
@@ -55,11 +54,6 @@ const Header = () => {
         {
             separator: true
         },
-        {
-            icon:"pi pi-search",
-            url:'/search',
-
-        },
     ];
 
     const start = <img alt="logo" src={logo}  height="70px"  className="p-mr-2"></img>;
@@ -76,48 +70,3 @@ const Header = () => {
 }
 
 export default Header;
-/*import React from 'react'
-import {ReactNavbar} from 'overlay-navbar';
-import logo from '../../../images/ecommerce.png'
-const options={
-    burgerColorHover:"#a62d24",
-    logo,
-     logoWidth:"7vmax",
-     logoHoverSize:"10px",
-     logoHoverColor:"#eb4034",
-     navColor1:"white",
-      link1Text:"Home",
-       link2Text :"Products",
-       link3Text:"Contact",
-            link4Text :"About",
-            link1Url :"/",
-            link2Url:"/products",
-             link3Url:"/contact",
-              link4Url:"/about",
-              linkSize:"1.3vmax",
-              linkColor:"rgba(35,35,35,0.8)",
-              nav1justifyContent:"flex-end",
-              nav2justifyContent:"flex-end",
-              nav3justifyContent:"flex-start",
-              nav4justifyContent:"flex-start",
-              link1ColorHover:"#eb4034",
-              link1Margin:"1vmax",
-              profileIconUrl:"/login",
-              profileIconColor:"rgba(35,35,35,0.8)",
-              searchIconColor:"rgba(35,35,35,0.8)",
-              cartIconColor:"rgba(35,35,35,0.8)",
-              searchIconColorHover:"#eb4034",
-              cartIconColorHover:"#eb4034",
-              profileIconColorHover:"#eb4034",
-              cartIconMargin:"1vmax",
-
-}
-const Header=()=> {
-    return (
-        <div>
-            <ReactNavbar {...options} />
-        </div>
-    )
-}
-
-export default Header;*/
